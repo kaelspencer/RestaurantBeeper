a9f8d8f64c2e6e99ffe6fa71bcdfecd8f8efe325
Bryan Wintermute <bryanwintermute@gmail.com>
2012-09-14 22:27:42 -0700

    Add QR Scanning support to WP app
    Add QR scanning to the title page. Currently just show the result in a
    TextBlock underneath the QR preview.

--- wp/RestaurantBeeper/RestaurantBeeper/MainPage.xaml.cs
+++ wp/RestaurantBeeper/RestaurantBeeper/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 
 namespace RestaurantBeeper
 {
@@ -23,6 +24,19 @@ namespace RestaurantBeeper
             // Set the data context of the listbox control to the sample data
             DataContext = App.ViewModel;
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+            //ApplicationBar = new ApplicationBar();
+            //ApplicationBar.Mode = ApplicationBarMode.Default;
+            //ApplicationBar.IsVisible = true;
+            //ApplicationBar.IsMenuEnabled = true;
+
+            //ApplicationBarIconButton button1 = new ApplicationBarIconButton();
+            //button1.IconUri = new Uri("/Images/appbar.add.rest.png", UriKind.Relative);
+            //button1.Text = "New";
+            //ApplicationBar.Buttons.Add(button1);
+
+            this.qrScanner.Visibility = Visibility.Collapsed;
+            this.qrScanner.StopScanning();
         }
 
         // Load data for the ViewModel Items
@@ -32,6 +46,35 @@ namespace RestaurantBeeper
             {
                 App.ViewModel.LoadData();
             }
+        }
+
+        private void qrButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch(this.qrScanner.Visibility)
+            {
+                case Visibility.Collapsed:
+                    // The control was hidden, make it visible and start
+                    this.qrScanner.Visibility = Visibility.Visible;
+                    this.qrScanner.StartScanning();
+                    break;
+                case Visibility.Visible:
+                    this.qrScanner.StopScanning();
+                    this.qrScanner.Visibility = Visibility.Collapsed;
+                    // The control was already visible... Do something?
+                    break;
+            }
+        }
+
+        private void QRCodeScanner_ScanComplete(object sender, JeffWilcox.Controls.ScanCompleteEventArgs e)
+        {
+            this.textBlockResult.Text = e.Result;
+            this.qrScanner.StopScanning();
+            this.qrScanner.StartScanning();
+        }
+
+        private void QRCodeScanner_Error(object sender, JeffWilcox.Controls.ScanFailureEventArgs e)
+        {
+            MessageBox.Show("GTFO");
         }
     }
 }
