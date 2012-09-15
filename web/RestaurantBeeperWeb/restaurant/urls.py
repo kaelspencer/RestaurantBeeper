5ef2917d2977770d0e59367590b91bebaf599731
kaelspencer <kael@descartes>
2012-09-15 09:45:01 -0700

    Change Poll URL to /retrieve/
    Change the poll url from /get/ to /retrieve/

--- web/RestaurantBeeperWeb/restaurant/urls.py
+++ web/RestaurantBeeperWeb/restaurant/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>[a-zA-Z0-9]{20})/$', views.get_visitor_json),
+    url(r'^retrieve/(?P<slug>[a-zA-Z0-9]{20})/$', views.get_visitor_json),
 )
